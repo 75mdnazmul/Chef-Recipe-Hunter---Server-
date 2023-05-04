e957f7f8c5fb77c8125a900b9b05e4d9eba2cf3e
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-04 10:40:58 +0600

    chef data configured

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
-
+const cors = require("cors");
 const app = express();
+app.use(cors())
+
+const chafData = require("./data.json")
 
 app.get('/', (req, res)=>{
     res.json({ message: "hello server running"})
 })
 
-app.get('/alldata', (req, res)=>{
-    console.log('hello ');
+app.get('/chefAllData', (req, res)=>{
+    res.send(chafData)
+})
+app.get('/chefAllData/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const selectedChefData = chafData.find(n => parseInt(n.id) === id)
+    console.log(selectedChefData);
+    res.send(selectedChefData);
 })
 
 app.listen(7000, ()=>{
